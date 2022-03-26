441130505223ba84136c1dabe9798cecd88bc144
Pavit <pavitpim@gmail.com>
2022-03-26 10:42:22 +0700

    condition

--- main.go
+++ main.go
@@ -14,9 +14,28 @@ func main(){
 // fmt.Printf("Hello go %t %t %d %d", isActive,active,num1, num2)
 
 // Accept Input
-fmt.Print("Input number");
-var num3 float64
-fmt.Scanf("%f", &num3)
-fmt.Printf("%.2f", num3)
+// fmt.Print("Input number");
+// var num3 float64
+// fmt.Scanf("%f", &num3)
+// fmt.Printf("%.2f", num3)
 
+// Condition 
+
+	var point float64
+	fmt.Print("Input point: ")
+	fmt.Scanf("%f", &point);
+
+	if point >= 80 {
+		fmt.Println("A")	
+	} else if point >= 70 {
+		fmt.Println("B")
+	} else if point >= 60 {
+		fmt.Println("C")
+	} else if point >= 50 {
+		fmt.Println("D")
+	} else if point < 0 {
+		fmt.Println("Invalid point")
+	} else {
+		fmt.Println("F")
+	}
 }
