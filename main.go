c933cbcd68208e47a9afc8778438d0fe4dbb1128
Pavit <pavitpim@gmail.com>
2022-03-26 10:34:41 +0700

    accept input

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import ("fmt")
 
 func main(){
-var num1,num2 int64
-// var num3,num4 float64
-// var str string
-// str2 := ""
-num2 = 10000
-var isActive bool
-active := true
-// fmt.Print("Hello go ", isActive, " ",active, num1, num2)
-fmt.Printf("Hello go %t %t %d %d", isActive,active,num1, num2)
+// var num1,num2 int64
+// // var num3,num4 float64
+// // var str string
+// // str2 := ""
+// num2 = 10000
+// var isActive bool
+// active := true
+// // fmt.Print("Hello go ", isActive, " ",active, num1, num2)
+// fmt.Printf("Hello go %t %t %d %d", isActive,active,num1, num2)
+
+// Accept Input
+fmt.Print("Input number");
+var num3 float64
+fmt.Scanf("%f", &num3)
+fmt.Printf("%.2f", num3)
 
 }
